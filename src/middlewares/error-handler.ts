1a8c3eafd3daf3df774bca35a18e09b07b51c918
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-25 19:07:35 +0300

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -4,16 +4,7 @@ import { CustomError } from "../errors/custom-error";
 export const errorHandler = (err: Error, req: Request, res: Response) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  } else {
-    res.status(400).send({
-      errors: [
-        {
-          message: `Something went wrong: ${
-            process.env.NODE_ENV === "development" ? err : ""
-          }`,
-        },
-      ],
-    });
   }
+  console.log(err);
   res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
