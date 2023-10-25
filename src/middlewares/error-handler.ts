b4e9e4ccdd9dd771aeeb02ce65f13eb06714469f
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-25 18:42:05 +0300

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -5,6 +5,6 @@ export const errorHandler = (err: Error, req: Request, res: Response) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-
+  console.error(err);
   res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
