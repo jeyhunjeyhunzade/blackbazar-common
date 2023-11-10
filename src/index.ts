e5326f6b202d1df11fb560a5cf9e324463d6dd42
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-11 00:17:38 +0300

    added order status enum

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,4 @@ export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-created-event";
 export * from "./events/ticket-updated-event";
+export * from "./events/types/order-status";
