875640d05f810ec9ce5b9b7f08c25db1db7bee27
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-15 21:11:59 +0300

    changed not-found-error message

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -4,7 +4,7 @@ export class NotFoundError extends CustomError {
   statusCode = 404;
 
   constructor() {
-    super("Route not found!");
+    super("Error 404 Not Found");
 
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
